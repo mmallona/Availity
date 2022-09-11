6547c54d09e22f42d99801e04858239a56068158
drussus@gmail.com <drussus@gmail.com>
2022-09-11 15:31:42 -0500

    program

---
+++ EnrolleeFiltering/Program.cs
@@ -0,0 +1,17 @@
+﻿using EnrolleeFiltering.BL;
+using EnrolleeFiltering.Interfaces;
+using System;
+
+namespace EnrolleeFiltering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IExecutor driver = new ClientDriver();
+            driver.Execute();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
