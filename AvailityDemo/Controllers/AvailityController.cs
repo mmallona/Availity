16b023fbd5b0e53f4bb76848e64ca9a3ad9cff1a
drussus@gmail.com <drussus@gmail.com>
2022-09-11 15:26:31 -0500

    Web

---
+++ AvailityDemo/Controllers/AvailityController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AvailityDemo.Controllers
+{
+    public class AvailityController : Controller
+    {
+        public IActionResult NewCustomerForm()
+        {            
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Success() //string message
+        {
+            return View();
+        }
+    }
+}
---
+++ AvailityDemo/wwwroot/js/site.js
@@ -0,0 +1,26 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(function () {
+
+    $("#btnRegister").submit( function (e) {
+         
+        e.preventDefault();
+
+        var url = "Success";    
+        $.ajax({
+            async: true,
+            type: "get",
+            data : Msg,
+            url: "Success",
+            dataType: "text",
+            success: function (response) {
+
+            }
+        });
+
+    });
+
+});
